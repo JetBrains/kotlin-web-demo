64220c5af55a10ae28ee5ee66389593803c46393
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2012-05-29 13:00:21 +0400

    Move patch files to jet.internal package

---
+++ idea.patch.applet/src/org/jetbrains/jet/internal/com/intellij/openapi/vfs/impl/jar/CoreJarHandler.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.jet.internal.com.intellij.openapi.vfs.impl.jar;
+
+import org.jetbrains.jet.internal.com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yole
+ */
+public class CoreJarHandler extends JarHandlerBase {
+    private final Map<String, VirtualFile> myFileMap = new HashMap<String, VirtualFile>();
+    private final CoreJarFileSystem myFileSystem;
+    private final VirtualFile myRoot;
+
+    public CoreJarHandler(CoreJarFileSystem fileSystem, String path) {
+        super(fileSystem, path);
+        myFileSystem = fileSystem;
+
+        Map<EntryInfo, CoreJarVirtualFile> entries = new HashMap<EntryInfo, CoreJarVirtualFile>();
+
+        for (EntryInfo info : getEntriesMap().values()) {
+            getOrCreateFile(info, entries);
+        }
+        myRoot = getOrCreateFile(getEntryInfo(""), entries);
+    }
+
+    private CoreJarVirtualFile getOrCreateFile(EntryInfo info, Map<EntryInfo, CoreJarVirtualFile> entries) {
+        CoreJarVirtualFile answer = entries.get(info);
+        if (answer == null) {
+            EntryInfo parentEntry = info.parent;
+            answer = new CoreJarVirtualFile(this, info, parentEntry != null ? getOrCreateFile(parentEntry, entries) : null);
+            entries.put(info, answer);
+        }
+
+        return answer;
+    }
+
+    @Nullable
+     public VirtualFile findFileByPath(String pathInJar) {
+        return myRoot != null ? myRoot.findFileByRelativePath(pathInJar) : null;
+    }
+
+    /*@Nullable
+    public VirtualFile findFileByPath(String pathInJar) {
+        if (getZip() == null) {
+            return null;
+        }
+        VirtualFile file = myFileMap.get(pathInJar);
+        if (file == null) {
+            EntryInfo entryInfo = null;
+            if (pathInJar.length() > 0) {
+                entryInfo = getEntryInfo(pathInJar);
+                if (entryInfo == null) {
+                    return null;
+                }
+            }
+            file = new CoreJarVirtualFile(this, entryInfo, pathInJar);
+            myFileMap.put(pathInJar, file);
+        }
+        return file;
+    }*/
+
+    public CoreJarFileSystem getFileSystem() {
+        return myFileSystem;
+    }
+}
