bc14bfa3684a0cd7788dd1271182a755cf294f64
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2012-05-29 14:31:31 +0400

    Change applet version

--- kotlin.web.demo.server/static/components/common.js
+++ kotlin.web.demo.server/static/components/common.js
@@ -24,7 +24,7 @@
 var BEFORE_EXIT = "The changes you made to the program will be lost when you change an example. Do you want to leave the page?";
 
 var KOTLIN_VERSION = "0.0.0";
-var APPLET_VERSION = "17042012";
+var APPLET_VERSION = "28052012";
 var WEB_DEMO_VERSION = "0.0.0";
 
 function forEachInArrayWithArgs(arr, data, f) {
