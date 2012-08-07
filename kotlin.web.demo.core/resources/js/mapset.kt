2e3a845e28bc33dcd20e39c8475e26e094b2329a
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2012-08-07 11:36:40 +0400

    Add hack to work HTMLBuilder example in js

---
+++ kotlin.web.demo.core/resources/js/mapset.kt
@@ -0,0 +1,10 @@
+package kotlin
+
+import java.util.Map
+import js.native
+
+/** Provides [] access to maps */
+native("put")
+public fun <K, V> Map<K, V>.set(key : K, value : V): Unit {
+    this.put(key, value)
+}
