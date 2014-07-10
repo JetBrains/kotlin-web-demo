ba6582041cb9d955786915fc7002546673b27a1a
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2014-07-10 16:21:22 +0400

    Update to Kotlin M8 - decompiler: add ClassFileDecompilers extension point

--- kotlin.web.demo.core/src/org/jetbrains/webdemo/environment/EnvironmentManager.java
+++ kotlin.web.demo.core/src/org/jetbrains/webdemo/environment/EnvironmentManager.java
@@ -1,9 +1,23 @@
 package org.jetbrains.webdemo.environment;
 
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeRegistry;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Getter;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.SingleRootFileViewProvider;
+import com.intellij.psi.compiled.ClassFileDecompilers;
+import com.intellij.psi.compiled.ClsStubBuilder;
+import com.intellij.psi.stubs.PsiFileStub;
+import com.intellij.util.cls.ClsFormatException;
+import com.intellij.util.indexing.FileContent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jet.cli.jvm.compiler.JetCoreEnvironment;
 import org.jetbrains.webdemo.WebDemoLightClassGenerationSupport;
 
@@ -27,6 +41,10 @@ public abstract class EnvironmentManager {
 
             environment.getApplication().registerService(WebDemoLightClassGenerationSupport.class, new WebDemoLightClassGenerationSupport());
 
+            Extensions.getRootArea()
+                    .getExtensionPoint(ClassFileDecompilers.EP_NAME)
+                    .registerExtension(new WebDemoClassFileDecompilers());
+
             //throw new IllegalStateException("Environment should be initialized before");
         }
         return environment;
@@ -38,5 +56,62 @@ public abstract class EnvironmentManager {
 
     public Getter<FileTypeRegistry> getRegistry() {
         return registry;
+    }
+
+    private static class WebDemoClassFileDecompilers extends ClassFileDecompilers.Full {
+
+        private final ClsStubBuilder stubBuilder = new ClsStubBuilder() {
+            @Override
+            public int getStubVersion() {
+                return 0;
+            }
+
+            @Nullable
+            @Override
+            public PsiFileStub<?> buildFileStub(@NotNull FileContent fileContent) throws ClsFormatException {
+                return null;
+            }
+        };
+
+        @Override
+        public boolean accepts(@NotNull VirtualFile virtualFile) {
+            return virtualFile.getPath().contains("kotlin-runtime.jar");
+        }
+
+        @NotNull
+        @Override
+        public ClsStubBuilder getStubBuilder() {
+            return stubBuilder;
+        }
+
+        @NotNull
+        @Override
+        public FileViewProvider createFileViewProvider(@NotNull VirtualFile virtualFile, @NotNull PsiManager psiManager, boolean physical) {
+            return new WebDemoClassFileViewProvider(psiManager, virtualFile, physical);
+        }
+
+        private static class WebDemoClassFileViewProvider extends SingleRootFileViewProvider {
+            public WebDemoClassFileViewProvider(@NotNull PsiManager manager, @NotNull VirtualFile virtualFile, boolean eventSystemEnabled) {
+                super(manager, virtualFile, eventSystemEnabled);
+            }
+
+            @NotNull
+            @Override
+            public CharSequence getContents() {
+                return "";
+            }
+
+            @Nullable
+            @Override
+            protected PsiFile createFile(@NotNull Project project, @NotNull VirtualFile file, @NotNull FileType fileType) {
+                return null;
+            }
+
+            @NotNull
+            @Override
+            public SingleRootFileViewProvider createCopy(@NotNull VirtualFile copy) {
+                return new WebDemoClassFileViewProvider(getManager(), copy, false);
+            }
+        }
     }
 }
