2012caa46440c29d49ffd66160d6677e59328efd
satamas <se.atamas@gmail.com>
2014-08-22 13:04:05 +0400

    Sum example fix

---
+++ examples/Problems/Sum/Test.kt
@@ -0,0 +1,17 @@
+package sum
+
+import org.junit.Test
+import kotlin.test.*
+
+fun my_fun() : Int{
+    return 0
+}
+
+public class TestSum {
+
+
+    Test fun testSum() {
+        var i  = 0
+        assertEquals("Hello world!", "hello world!".capitalize())
+    }
+}
