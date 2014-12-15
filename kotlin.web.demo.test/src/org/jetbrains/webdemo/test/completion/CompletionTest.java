5df76a1be37cbefe58ccc36b38cfcf9987ab7710
satamas <se.atamas@gmail.com>
2014-12-15 18:08:35 +0300

    Completion tests2

--- kotlin.web.demo.test/src/org/jetbrains/webdemo/test/completion/CompletionTest.java
+++ kotlin.web.demo.test/src/org/jetbrains/webdemo/test/completion/CompletionTest.java
@@ -16,6 +16,9 @@
 
 package org.jetbrains.webdemo.test.completion;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.webdemo.JetPsiFactoryUtil;
 import org.jetbrains.webdemo.responseHelpers.JsonResponseForCompletion;
@@ -23,87 +26,91 @@ import org.jetbrains.webdemo.session.SessionInfo;
 import org.jetbrains.webdemo.test.BaseTest;
 import org.jetbrains.webdemo.test.TestUtils;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class CompletionTest extends BaseTest {
 
-    public void test$java$system() throws IOException, JSONException {
-        String expectedResult = "[{\"icon\":\"class\",\"name\":\"System\",\"tail\":\" (java.lang)\"},{\"icon\":\"class\",\"name\":\"SystemClassLoaderAction\",\"tail\":\" (java.lang)\"},{\"icon\":\"package\",\"name\":\"System\",\"tail\":\"package java.lang.System\"}]";
+    public void test$java$system() throws IOException {
+        String expectedResult = "[{\"icon\":\"class\",\"name\":\"System\",\"tail\":\" (java.lang)\"},{\"icon\":\"class\",\"name\":\"SystemClassLoaderAction\",\"tail\":\" (java.lang)\"}]";
         compareResult(1, 6, expectedResult, "java");
     }
 
-    public void test$java$out() throws IOException, JSONException {
-        String expectedResult = "[{\"icon\":\"property\",\"name\":\"out\",\"tail\":\"PrintStream\"}]";
+    public void test$java$out() throws IOException {
+        String expectedResult = "[{\"icon\":\"property\",\"name\":\"out\",\"tail\":\"PrintStream?\"}]";
         compareResult(1, 10, expectedResult, "java");
     }
 
-    public void test$all$variable() throws IOException, JSONException {
-        String expectedResult = "[{\"icon\":\"property\",\"name\":\"stdin\",\"tail\":\"BufferedReader\"},{\"icon\":\"method\",\"name\":\"stdlib_emptyList()\",\"tail\":\"List<T>\"},{\"icon\":\"property\",\"name\":\"stdlib_emptyList\",\"tail\":\"List<Any>\"},{\"icon\":\"method\",\"name\":\"stdlib_emptyMap()\",\"tail\":\"Map<K, V>\"},{\"icon\":\"property\",\"name\":\"stdlib_emptyMap\",\"tail\":\"Map<Any, Any>\"},{\"icon\":\"method\",\"name\":\"stream(initialValue: T, nextFunction: (T...\",\"tail\":\"Stream<T>\"},{\"icon\":\"method\",\"name\":\"stream(nextFunction: () -> T?)\",\"tail\":\"Stream<T>\"},{\"icon\":\"method\",\"name\":\"streamOf(vararg elements: T)\",\"tail\":\"Stream<T>\"},{\"icon\":\"property\",\"name\":\"str\",\"tail\":\"String\"}]";
+    public void test$all$variable() throws IOException {
+        String expectedResult = "[{\"icon\":\"property\",\"name\":\"stdin\",\"tail\":\"BufferedReader\"},{\"icon\":\"method\",\"name\":\"stdlib_emptyList()\",\"tail\":\"List<T>\"},{\"icon\":\"property\",\"name\":\"stdlib_emptyList\",\"tail\":\"List<Any>\"},{\"icon\":\"method\",\"name\":\"stdlib_emptyMap()\",\"tail\":\"Map<K, V>\"},{\"icon\":\"property\",\"name\":\"stdlib_emptyMap\",\"tail\":\"Map<Any, Any>\"},{\"icon\":\"method\",\"name\":\"stream(() -> T?)\",\"tail\":\"Stream<T>\"},{\"icon\":\"method\",\"name\":\"stream(T, (T) -> T?)\",\"tail\":\"Stream<T>\"},{\"icon\":\"method\",\"name\":\"streamOf(Progression<T>)\",\"tail\":\"Stream<T>\"},{\"icon\":\"method\",\"name\":\"streamOf(vararg T)\",\"tail\":\"Stream<T>\"},{\"icon\":\"property\",\"name\":\"str\",\"tail\":\"String\"}]";
         compareResult(14, 24, expectedResult, "java");
         compareResult(14, 24, expectedResult, "js");
     }
 
-    public void test$all$class() throws IOException, JSONException {
+    public void test$all$class() throws IOException {
         String expectedResult = "[{\"icon\":\"class\",\"name\":\"Greeter\",\"tail\":\" (<root>)\"}]";
         compareResult(4, 3, expectedResult, "java");
         compareResult(4, 3, expectedResult, "js");
     }
 
-    public void test$all$type$in$constructor() throws IOException, JSONException {
-        String expectedResult = "[{\"icon\":\"class\",\"name\":\"StrictMath\",\"tail\":\" (java.lang)\"},{\"icon\":\"package\",\"name\":\"StrictMath\",\"tail\":\"package java.lang.StrictMath\"},{\"icon\":\"class\",\"name\":\"String\",\"tail\":\" (kotlin)\"},{\"icon\":\"method\",\"name\":\"String(bytes: ByteArray)\",\"tail\":\"String\"},{\"icon\":\"method\",\"name\":\"String(bytes: ByteArray, charset: Charse...\",\"tail\":\"String\"},{\"icon\":\"method\",\"name\":\"String(bytes: ByteArray, charsetName: St...\",\"tail\":\"String\"},{\"icon\":\"method\",\"name\":\"String(bytes: ByteArray, i: Int, i1: Int...\",\"tail\":\"String\"},{\"icon\":\"method\",\"name\":\"String(bytes: ByteArray, offset: Int, le...\",\"tail\":\"String\"},{\"icon\":\"method\",\"name\":\"String(bytes: ByteArray, offset: Int, le...\",\"tail\":\"String\"},{\"icon\":\"method\",\"name\":\"String(chars: CharArray)\",\"tail\":\"String\"},{\"icon\":\"method\",\"name\":\"String(stringBuffer: StringBuffer)\",\"tail\":\"String\"},{\"icon\":\"method\",\"name\":\"String(stringBuilder: StringBuilder)\",\"tail\":\"String\"},{\"icon\":\"class\",\"name\":\"StringBuffer\",\"tail\":\" (java.lang)\"},{\"icon\":\"package\",\"name\":\"StringBuffer\",\"tail\":\"package java.lang.StringBuffer\"},{\"icon\":\"class\",\"name\":\"StringBuilder\",\"tail\":\" (java.lang)\"},{\"icon\":\"method\",\"name\":\"StringBuilder(body: StringBuilder.() -> ...\",\"tail\":\"StringBuilder\"},{\"icon\":\"package\",\"name\":\"StringBuilder\",\"tail\":\"package java.lang.StringBuilder\"},{\"icon\":\"class\",\"name\":\"StringCoding\",\"tail\":\" (java.lang)\"},{\"icon\":\"package\",\"name\":\"StringCoding\",\"tail\":\"package java.lang.StringCoding\"},{\"icon\":\"class\",\"name\":\"StringIndexOutOfBoundsException\",\"tail\":\" (java.lang)\"},{\"icon\":\"class\",\"name\":\"StringValue\",\"tail\":\" (java.lang)\"},{\"icon\":\"package\",\"name\":\"StringValue\",\"tail\":\"package java.lang.StringValue\"}]";
+    public void test$all$type$in$constructor() throws IOException {
+        String expectedResult = "[{\"icon\":\"class\",\"name\":\"StrictMath\",\"tail\":\" (java.lang)\"},{\"icon\":\"class\",\"name\":\"String\",\"tail\":\" (kotlin)\"},{\"icon\":\"class\",\"name\":\"StringBuffer\",\"tail\":\" (java.lang)\"},{\"icon\":\"class\",\"name\":\"StringBuilder\",\"tail\":\" (java.lang)\"},{\"icon\":\"class\",\"name\":\"StringCoding\",\"tail\":\" (java.lang)\"},{\"icon\":\"class\",\"name\":\"StringIndexOutOfBoundsException\",\"tail\":\" (java.lang)\"}]";
         compareResult(12, 28, expectedResult, "java");
         compareResult(12, 28, expectedResult, "js");
     }
 
-    public void test$java$println() throws IOException, JSONException {
-        String expectedResult = "[{\"icon\":\"method\",\"name\":\"print(Any?)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Boolean)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Char)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(CharArray)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Double)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Float)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Int)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Long)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(String?)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"printf(Locale?, String, vararg Any?)\",\"tail\":\"PrintStream\"},{\"icon\":\"method\",\"name\":\"printf(String, vararg Any?)\",\"tail\":\"PrintStream\"},{\"icon\":\"method\",\"name\":\"println()\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Any?)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Boolean)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Char)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(CharArray)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Double)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Float)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Int)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Long)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(String?)\",\"tail\":\"Unit\"}]";
+    public void test$java$println() throws IOException {
+        String expectedResult = "[{\"icon\":\"method\",\"name\":\"print(Any?)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Boolean)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Char)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(CharArray?)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Double)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Float)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Int)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(Long)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"print(String?)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"printf(Locale?, String, vararg Any?)\",\"tail\":\"PrintStream\"},{\"icon\":\"method\",\"name\":\"printf(String, vararg Any?)\",\"tail\":\"PrintStream\"},{\"icon\":\"method\",\"name\":\"println()\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Any?)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Boolean)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Char)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(CharArray?)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Double)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Float)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Int)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(Long)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"println(String?)\",\"tail\":\"Unit\"}]";
 
         compareResult(1, 15, expectedResult, "java");
     }
 
-    public void test$js$a() throws IOException, JSONException {
-        String expectedResult = "[{\"icon\":\"package\",\"name\":\"annotation\",\"tail\":\"package java.lang.annotation\"},{\"icon\":\"genericValue\",\"name\":\"args\",\"tail\":\"Array<String>\"},{\"icon\":\"method\",\"name\":\"array(vararg t: T)\",\"tail\":\"Array<T>\"},{\"icon\":\"method\",\"name\":\"arrayList(vararg values: T)\",\"tail\":\"ArrayList<T>\"},{\"icon\":\"method\",\"name\":\"arrayListOf(vararg values: T)\",\"tail\":\"ArrayList<T>\"},{\"icon\":\"method\",\"name\":\"arrayOfNulls(size: Int)\",\"tail\":\"Array<T?>\"},{\"icon\":\"method\",\"name\":\"assert(value: Boolean, lazyMessage: () -...\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"assert(value: Boolean, message: Any = .....\",\"tail\":\"Unit\"}]";
+    public void test$js$a() throws IOException {
+        String expectedResult = "[{\"icon\":\"package\",\"name\":\"annotation\",\"tail\":\"package java.lang.annotation\"},{\"icon\":\"genericValue\",\"name\":\"args\",\"tail\":\"Array<String>\"},{\"icon\":\"method\",\"name\":\"array(vararg T)\",\"tail\":\"Array<T>\"},{\"icon\":\"method\",\"name\":\"arrayList(vararg T)\",\"tail\":\"ArrayList<T>\"},{\"icon\":\"method\",\"name\":\"arrayListOf(vararg T)\",\"tail\":\"ArrayList<T>\"},{\"icon\":\"method\",\"name\":\"arrayOfNulls(Int)\",\"tail\":\"Array<T?>\"},{\"icon\":\"method\",\"name\":\"assert(Boolean, () -> String)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"assert(Boolean, Any)\",\"tail\":\"Unit\"}]";
 
         compareResult(2, 3, expectedResult, "js");
     }
 
-    public void test$kt$a() throws IOException, JSONException {
-        String expectedResult = "[{\"icon\":\"package\",\"name\":\"annotation\",\"tail\":\"package java.lang.annotation\"},{\"icon\":\"genericValue\",\"name\":\"args\",\"tail\":\"Array<String>\"},{\"icon\":\"method\",\"name\":\"array(vararg t: T)\",\"tail\":\"Array<T>\"},{\"icon\":\"method\",\"name\":\"arrayList(vararg values: T)\",\"tail\":\"ArrayList<T>\"},{\"icon\":\"method\",\"name\":\"arrayListOf(vararg values: T)\",\"tail\":\"ArrayList<T>\"},{\"icon\":\"method\",\"name\":\"arrayOfNulls(size: Int)\",\"tail\":\"Array<T?>\"},{\"icon\":\"method\",\"name\":\"assert(value: Boolean, lazyMessage: () -...\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"assert(value: Boolean, message: Any = .....\",\"tail\":\"Unit\"}]";
+    public void test$kt$a() throws IOException {
+        String expectedResult = "[{\"icon\":\"package\",\"name\":\"annotation\",\"tail\":\"package java.lang.annotation\"},{\"icon\":\"genericValue\",\"name\":\"args\",\"tail\":\"Array<String>\"},{\"icon\":\"method\",\"name\":\"array(vararg T)\",\"tail\":\"Array<T>\"},{\"icon\":\"method\",\"name\":\"arrayList(vararg T)\",\"tail\":\"ArrayList<T>\"},{\"icon\":\"method\",\"name\":\"arrayListOf(vararg T)\",\"tail\":\"ArrayList<T>\"},{\"icon\":\"method\",\"name\":\"arrayOfNulls(Int)\",\"tail\":\"Array<T?>\"},{\"icon\":\"method\",\"name\":\"assert(Boolean, () -> String)\",\"tail\":\"Unit\"},{\"icon\":\"method\",\"name\":\"assert(Boolean, Any)\",\"tail\":\"Unit\"}]";
         compareResult(2, 3, expectedResult, "java");
     }
 
-    private void compareResult(int start, int end, String expectedResult, String runConfiguration) throws IOException, JSONException {
+    private void compareResult(int lineNo, int charNo, String expectedResult, String runConfiguration) throws IOException {
         sessionInfo.setType(SessionInfo.TypeOfRequest.COMPLETE);
         sessionInfo.setRunConfiguration(runConfiguration);
-        PsiFile currentPsiFile = JetPsiFactoryUtil.createFile(getProject(), getProject().getName(), TestUtils.getDataFromFile(TestUtils.TEST_SRC, "completion/root.kt"));
+        PsiFile currentPsiFile = JetPsiFactoryUtil.createFile(getProject(), "completion/root.kt", TestUtils.getDataFromFile(TestUtils.TEST_SRC, "completion/root.kt"));
 
-        JsonResponseForCompletion responseForCompletion = new JsonResponseForCompletion(Collections.singletonList(currentPsiFile), sessionInfo, currentPsiFile.getName(), start, end);
+        JsonResponseForCompletion responseForCompletion = new JsonResponseForCompletion(new ArrayList<PsiFile>(Collections.singletonList(currentPsiFile)), sessionInfo, currentPsiFile.getName(), lineNo, charNo);
         String actualResult = responseForCompletion.getResult();
-        /*JSONArray expectedArray = new JSONArray(expectedResult);
-        JSONArray actualArray = new JSONArray(actualResult);
 
-//        assertEquals(expectedResult, actualResult);
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayNode expectedArray = (ArrayNode) objectMapper.readTree(expectedResult);
+        ArrayNode actualArray = (ArrayNode) objectMapper.readTree(actualResult);
 
-        assertEquals("Incorrect count of objects in completion", expectedArray.length(), actualArray.length());
+        assertEquals(expectedResult, actualResult);
+
+        assertEquals("Incorrect count of objects in completion", expectedArray.size(), actualArray.size());
 
 
-        for (int i = 0; i < expectedArray.length(); i++) {
-            JSONObject expectedObject = (JSONObject) expectedArray.get(i);
-            for (int j = 0; j < actualArray.length(); j++) {
-                if (expectedObject.get("name").equals(((JSONObject) actualArray.get(j)).get("name"))) {
+        for (int i = 0; i < expectedArray.size(); i++) {
+            ObjectNode expectedObject = (ObjectNode) expectedArray.get(i);
+            for (int j = 0; j < actualArray.size(); j++) {
+                if (expectedObject.get("name").equals(actualArray.get(j).get("name"))) {
                     assertEquals(expectedObject.toString(), actualArray.get(j).toString());
                     break;
                 } else {
-                    if (j == actualArray.length() - 1) {
+                    if (j == actualArray.size() - 1) {
                         assertEquals("Cannot find element for " + expectedObject.toString(), true, false);
                     }
                 }
             }
-        }*/
+        }
 
-        assertEquals("Wrong result: " + start + ", " + end, expectedResult, actualResult);
+        assertEquals("Wrong result: " + lineNo + ", " + charNo, expectedResult, actualResult);
     }
 
 }
