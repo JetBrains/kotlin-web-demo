ecbbe7adadb0251c8406326ee7e029942b361fe7
satamas <se.atamas@gmail.com>
2015-03-02 12:23:56 +0300

    Change timeout from 1000s to 1s

--- kotlin.web.demo.server/src/org/jetbrains/webdemo/database/MySqlConnector.java
+++ kotlin.web.demo.server/src/org/jetbrains/webdemo/database/MySqlConnector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2012 JetBrains s.r.o.
+ * Copyright 2000-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,7 +76,7 @@ public class MySqlConnector {
 
     private void checkConnection() throws DatabaseOperationException {
         try {
-            if (!connection.isValid(1000) && !connect()) {
+            if (!connection.isValid(1) && !connect()) {
                 throw new DatabaseOperationException("Can't connect to database");
             }
         } catch (SQLException e) {
