b34f37f9d3e2b520d273ec961f8c59416e0faad2
satamas <se.atamas@gmail.com>
2015-06-17 11:49:16 +0300

    Update db schema for workshop projects.

--- kotlin.web.demo.server/resources/db_schema.sql
+++ kotlin.web.demo.server/resources/db_schema.sql
@@ -14,14 +14,24 @@ CREATE TABLE IF NOT EXISTS projects (
   id                INT                                    NOT NULL PRIMARY KEY AUTO_INCREMENT,
   public_id         VARCHAR(45)                            NOT NULL UNIQUE,
   owner_id          INT                                    NOT NULL,
+  FOREIGN KEY (owner_id) REFERENCES users (id)
+    ON DELETE CASCADE,
   name              VARCHAR(100)                           NOT NULL             DEFAULT '',
   args              VARCHAR(45)                            NOT NULL             DEFAULT '',
   run_configuration ENUM ('java', 'js', 'canvas', 'junit') NOT NULL             DEFAULT 'java',
   origin            VARCHAR(100),
   read_only_files   TEXT                                   NOT NULL,
-  CONSTRAINT project_name UNIQUE (owner_id, name),
-  FOREIGN KEY (owner_id) REFERENCES users (id)
-    ON DELETE CASCADE
+  type    ENUM('USER_PROJECT', 'KOANS_TASK', 'INCOMPLETE_KOANS_TASK') NOT NULL DEFAULT 'USER_PROJECT',
+  task_id INT                                                                  DEFAULT NULL, #NULL for user projects
+  FOREIGN KEY (task_id) REFERENCES koans_tasks (id)
+    ON DELETE RESTRICT,
+  CONSTRAINT UNIQUE (owner_id, name, type),
+  CONSTRAINT UNIQUE (owner_id, task_id)
+);
+
+CREATE TABLE IF NOT EXISTS koans_tasks (
+  id        INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  public_id VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS files (
