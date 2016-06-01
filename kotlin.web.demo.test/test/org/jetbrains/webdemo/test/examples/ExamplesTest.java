84bcf8b11eb8a083f4df8ae19d6bd5254eee3e90
satamas <se.atamas@gmail.com>
2016-06-01 17:06:08 +0300

    Move run examples test to ExamplesTest

--- kotlin.web.demo.test/test/org/jetbrains/webdemo/test/examples/ExamplesTest.java
+++ kotlin.web.demo.test/test/org/jetbrains/webdemo/test/examples/ExamplesTest.java
@@ -17,40 +17,37 @@
 package org.jetbrains.webdemo.test.examples;
 
 import org.jetbrains.webdemo.ProjectFile;
+import org.jetbrains.webdemo.backend.executor.ExecutorUtils;
+import org.jetbrains.webdemo.backend.executor.result.ExecutionResult;
+import org.jetbrains.webdemo.backend.executor.result.JavaExecutionResult;
+import org.jetbrains.webdemo.backend.executor.result.JunitExecutionResult;
+import org.jetbrains.webdemo.backend.executor.result.TestRunInfo;
 import org.jetbrains.webdemo.examples.Example;
 import org.jetbrains.webdemo.examples.ExamplesFolder;
 import org.jetbrains.webdemo.examples.ExamplesUtils;
+import org.jetbrains.webdemo.kotlin.datastructures.CompilationResult;
 import org.jetbrains.webdemo.kotlin.datastructures.ErrorDescriptor;
 import org.jetbrains.webdemo.kotlin.datastructures.TextPosition;
 import org.jetbrains.webdemo.test.BaseTest;
+import org.jetbrains.webdemo.test.Common;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RunWith(Parameterized.class)
 public class ExamplesTest extends BaseTest {
-
-    private static ArrayList<String> jsExamples = new ArrayList<String>();
     private final Example project;
-    private String runConfiguration;
-
 
     public ExamplesTest(Example project) {
         super(project.name);
         this.project = project;
-        this.runConfiguration = project.confType;
     }
 
-//    public HighlightExamplesTest(Example project, String runConfiguration) {
-//        super(project.name + "_" + runConfiguration);
-//        this.project = project;
-//        this.runConfiguration = runConfiguration;
-//    }
-
-//    @Override
     @Test
     public void testHighlighting() throws Throwable {
         Map<String, String> files = new HashMap<>();
@@ -66,6 +63,58 @@ public class ExamplesTest extends BaseTest {
         for (String fileName : result.keySet()) {
             assertTrue(getErrorsMessages(fileName, result.get(fileName)), result.get(fileName).isEmpty());
         }
+    }
+
+    @Test
+    public void testRun() throws Exception {
+        Map<String, String> files = new HashMap<>();
+        for (ProjectFile file : project.files) {
+            files.put(file.getName(), file.getText());
+        }
+        for (ProjectFile file : project.getHiddenFiles()) {
+            files.put(file.getName(), file.getText());
+        }
+
+        if (project.confType.equals("java")) {
+            JavaExecutionResult executionResult = (JavaExecutionResult) compileAndExecute(files, project.args, false);
+            assertEquals(unifyLineSeparators(project.expectedOutput), getStdOut(executionResult.getText()));
+            assertNull(executionResult.getException());
+        } else if (project.confType.equals("junit")) {
+            JunitExecutionResult executionResult = (JunitExecutionResult) compileAndExecute(files, project.args, true);
+            assertFalse("No test results", executionResult.getTestResults().isEmpty());
+            for (String className : executionResult.getTestResults().keySet()) {
+                for(TestRunInfo testRunInfo : executionResult.getTestResults().get(className)) {
+                    String message = testRunInfo.getClassName() + "." + testRunInfo.getMethodName() + " status:" + testRunInfo.getStatus();
+                    assertEquals(message, TestRunInfo.Status.OK, testRunInfo.getStatus());
+                }
+            }
+        }
+    }
+
+    private String getStdOut(String programOutput) {
+        programOutput = Common.unEscapeString(programOutput).replaceAll("\\r\\n", "</br>").replaceAll("\\n", "</br>");
+        StringBuilder builder = new StringBuilder();
+        Matcher matcher = Pattern.compile("<outStream>(.*)</outStream>").matcher(programOutput);
+        while (matcher.find()) {
+            builder.append(matcher.group(1));
+        }
+        return unifyLineSeparators(builder.toString());
+    }
+
+    private ExecutionResult compileAndExecute(Map<String, String> files, String args, boolean isJunit) throws Exception {
+        CompilationResult compilationResult = kotlinWrapper.compileCorrectFiles(files);
+        return ExecutorUtils.executeCompiledFiles(
+                compilationResult.getFiles(),
+                compilationResult.getMainClass(),
+                kotlinWrapper.getKotlinRuntimeLibraries(),
+                kotlinWrapper.getKotlinCompilerJar(),
+                args,
+                isJunit
+        );
+    }
+
+    private String unifyLineSeparators(String text) {
+        return text.replaceAll("\\r\\n", "</br>").replaceAll("\\n", "</br>").replaceAll("</br>", System.lineSeparator());
     }
 
     private String getErrorsMessages(String fileName, List<ErrorDescriptor> errors) {
