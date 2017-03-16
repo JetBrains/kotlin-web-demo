a1790a5b38b2073c2f31ad0025fde1c478386612
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2017-03-16 14:56:00 +0300

    Updated 'MyDate.kt'

--- kotlin.web.demo.server/examples/Kotlin Koans/Conventions/In range/MyDate.kt
+++ kotlin.web.demo.server/examples/Kotlin Koans/Conventions/In range/MyDate.kt
@@ -1,7 +1,7 @@
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate>{
-    override fun compareTo(other: MyDate): Int{
-        if(year != other.year) return year - other.year
-        if(month != other.month) return month - other.month
-        return dayOfMonth - other.dayOfMonth
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
     }
 }
