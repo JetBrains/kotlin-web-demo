8a754c69ec1478cce6c7b8e1f154b180cb33103e
Richard Green <richard.green@r3cev.com>
2017-11-22 16:47:55 +0000

    Added import

--- kotlin.web.demo.server/examples/Corda Coans/Introduction/Hello, Corda!/Task.kt
+++ kotlin.web.demo.server/examples/Corda Coans/Introduction/Hello, Corda!/Task.kt
@@ -7,14 +7,13 @@
 
 package net.corda.training.state
 
+/* These imports will probably give you quite a hint about the types that you'll need to use */
+
 import net.corda.core.contracts.ContractState
 import net.corda.core.identity.Party
 import net.corda.core.identity.AbstractParty
+import net.corda.core.contracts.Amount
 import java.util.Currency
-
-
-
-
 
 data class IOUState(<taskWindow>val data: String = "data"</taskWindow>): ContractState {
     override val participants: List<AbstractParty> get() = listOf()
