50c69bd7c35d83a3b906d47a1a88d638945cf780
Prendota <prendota@mail.ru>
2018-02-22 14:34:59 +0300

    add property source path

--- kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/Application.kt
+++ kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/Application.kt
@@ -2,11 +2,13 @@ package web.demo.server
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.PropertySource
 
 /**
  * Go go go
  */
 @SpringBootApplication
+@PropertySource(value = "classpath:application.yml")
 class Application {
 
     companion object {
