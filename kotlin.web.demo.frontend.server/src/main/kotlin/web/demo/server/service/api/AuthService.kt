5b4353a3e26c7a3fb2c00967748a86c1ec5fca68
Prendota <prendota@mail.ru>
2018-02-26 11:29:10 +0300

    Create authService and impl

---
+++ kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/service/api/AuthService.kt
@@ -0,0 +1,8 @@
+package web.demo.server.service.api
+
+/**
+ * @author Alexander Prendota on 2/26/18 JetBrains.
+ */
+interface AuthService {
+
+}
---
+++ kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/service/impl/AuthServiceImpl.kt
@@ -0,0 +1,5 @@
+package web.demo.server.service.impl
+
+import web.demo.server.service.api.AuthService
+
+class AuthServiceImpl : AuthService
