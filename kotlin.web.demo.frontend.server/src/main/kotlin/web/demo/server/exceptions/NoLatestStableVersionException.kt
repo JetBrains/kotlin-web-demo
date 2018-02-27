8ac124dd7e24855fbe7df1f759e23f5421e222d7
Prendota <Prendota@mail.ru>
2018-02-27 10:34:10 +0300

    Delete unused exception

--- kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/exceptions/NoLatestStableVersionException.kt
+++
@@ -1,6 +0,0 @@
-package web.demo.server.exceptions
-
-/**
- * @author Alexander Prendota on 2/6/18 JetBrains.
- */
-class NoLatestStableVersionException(override var message: String) : Exception(message)
