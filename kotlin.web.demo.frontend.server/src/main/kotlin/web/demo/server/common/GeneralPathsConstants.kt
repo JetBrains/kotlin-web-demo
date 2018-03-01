05a658135159313cdc61985fa5a2878225983b8b
Prendota <prendota@mail.ru>
2018-03-01 11:09:09 +0300

    add general paths constants

--- kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/common/GeneralPathsConstants.kt
+++ kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/common/GeneralPathsConstants.kt
@@ -1,5 +1,8 @@
 package web.demo.server.common
 
+import web.demo.server.entity.File
+import web.demo.server.entity.Project
+
 /**
  * General paths for web-demo server
  */
@@ -9,4 +12,26 @@ object GeneralPathsConstants {
      * Path for storage information about user
      */
     const val CURRENT_USER = "userInfo"
+
+    /**
+     * Basic operations
+     */
+    const val EXIST = "exist/"
+    const val DELETE = "delete/"
+    const val SAVE = "save/"
+    const val ADD = "add/"
+    const val RENAME = "rename/"
+
+    /**
+     * Path constants for operation with [File]
+     */
+    const val API_FILE = "api/file/"
+
+    /**
+     * Path constants for operation with [Project]
+     */
+    const val API_PROJECT = "api/project/"
+    const val USER_PROJECT_TYPE = "USER_PROJECT"
+    const val DEFAULT_PROJECT_NAME = "Project"
+
 }
