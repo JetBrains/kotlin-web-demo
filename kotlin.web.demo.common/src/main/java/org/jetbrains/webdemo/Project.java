2ab7b6867d2156322daa90257bc65c24dc6ffa0b
Prendota <prendota@mail.ru>
2018-03-07 15:21:58 +0300

    Add ignore properties

--- kotlin.web.demo.common/src/main/java/org/jetbrains/webdemo/Project.java
+++ kotlin.web.demo.common/src/main/java/org/jetbrains/webdemo/Project.java
@@ -17,11 +17,13 @@
 package org.jetbrains.webdemo;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Project {
     public String id;
     public String originUrl;
