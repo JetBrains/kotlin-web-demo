790910725e0e69ae607db16e25727b6a01b01e9e
Prendota <prendota@mail.ru>
2018-03-16 19:27:47 +0300

    Move db configuration to [ServerConfiguration]

--- kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/configuration/ServerConfiguration.kt
+++ kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/configuration/ServerConfiguration.kt
@@ -1,12 +1,16 @@
 package web.demo.server.configuration
 
 import org.modelmapper.ModelMapper
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
+import javax.sql.DataSource
 
 
 /**
@@ -56,5 +60,17 @@ class ServerConfiguration {
         config.addAllowedMethod("DELETE")
         source.registerCorsConfiguration("/**", config)
         return CorsFilter(source)
+    }
+
+    /**
+     * If your wanna start application not from Docker -> set database environment in application.yml
+     */
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    fun dataSource(): DataSource {
+        return DataSourceBuilder
+                .create()
+                .build()
     }
 }
