14aea4923fc28a342f35ebc0214d8c32debe3921
Prendota <prendota@mail.ru>
2018-03-23 12:19:13 +0300

    [StepikSubmissions] add submission and attempts wrappers

--- kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/model/stepik/StepikSubmission.kt
+++ kotlin.web.demo.frontend.server/src/main/kotlin/web/demo/server/model/stepik/StepikSubmission.kt
@@ -1,17 +1,29 @@
 package web.demo.server.model.stepik
 
 /**
+ * Stepik structure for operation with Attempts ans Submissions
+ *
+ * @see <a href="https://stepik.org/api/docs/#!/attempts">Stepik Attempts</a>
+ * @see <a href="https://stepik.org/api/docs/#!/submissions">Stepik Submissions</a>
+ *
  * @author Alexander Prendota on 3/20/18 JetBrains.
  */
 data class StepikSubmissionContainer(var submissions: List<StepikSubmission>)
 
-data class StepikSubmission(var reply: StepikReply)
+data class StepikSubmission(var reply: StepikReply,
+                            var attempt: String)
 
-data class StepikReply(var solution: List<StepikSolution>)
+data class SubmissionWrapper(var submission: StepikSubmission)
+
+data class StepikReply(var solution: List<StepikSolution>,
+                       var score: String)
 
 data class StepikSolution(var name: String,
                           var text: String)
 
 data class StepikAttemptContainer(var attempts: List<StepikAttempt>)
 
-data class StepikAttempt(var id: String)
+data class AttemptWrapper(var attempt: StepikAttempt)
+
+data class StepikAttempt(var id: String,
+                         var step: String)
